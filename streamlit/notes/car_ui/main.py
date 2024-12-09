bb611a9e16ade140bdb52a928f7af25940f77cc3
yooodleee <accia25@naver.com>
2024-12-09 09:46:01 +0900

    feat: [오민수] car_ui에 대한 로직 설명입니다. 참고하세요 [COTIRS-x]

--- streamlit/notes/car_ui/main.py
+++ streamlit/notes/car_ui/main.py
@@ -10,10 +10,13 @@ st.title("전기차 종합 DB 조회 포털:mag:")
 tab1, tab2, tab3 = st.tabs(["차량 정보 조회", "전기차 등록 대수 현황", "업체 별 전기차 충전 요금"])
 
 with tab1:
+    # "차량 정보 조회" 탭을 누르면 CarUi에 대한 정보가 나오겠군요~
     showCarUi()
 
 with tab2:
+    # "전기차 등록 대수 현황" 탭을 누르면 RegiosUi에 대한 정보가 나오겠군요~
     showRegionsUi()
 
 with tab3:
+    # "업체 별 전기차 충전 요금" 탭을 누르면 ChargingUi에 대한 정보가 나오겠군요~
     showChargingUi()
